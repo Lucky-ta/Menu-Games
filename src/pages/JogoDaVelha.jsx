76e7cc87c037a5438ba025c27298229a4e1a6e93
lucas <lucmaieski@gmail.com>
2022-06-23 04:44:22 -0300

    Get tictactoe table

--- src/pages/JogoDaVelha.jsx
+++ src/pages/JogoDaVelha.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../styles/TicTacToe.css';
 
 function JogoDaVelha() {
+  const table = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
   return (
     <div>
       <h1>JogoDaVelha</h1>
+      <div className="tictactoe-table">
+        {table.map((value) => (
+          <div className='table' key={value}>
+            <span>{value}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
