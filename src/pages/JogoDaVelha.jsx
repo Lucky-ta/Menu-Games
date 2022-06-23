36a7281269af3727353490ab68942622da9d67cd
lucas <lucmaieski@gmail.com>
2022-06-23 16:30:41 -0300

    Config result timeOut

--- src/pages/JogoDaVelha.jsx
+++ src/pages/JogoDaVelha.jsx
@@ -19,13 +19,54 @@ function JogoDaVelha() {
   const hanldeAttack = (id) => {
     if (side === null) return alert('Selecione um lado!');
     setPlayedPositions([...playedPositions, Number(id)]);
-    // console.log(positions);
     setRound(round + 1);
     return player.attack(id);
   };
 
+  const verifyTableResult = () => {
+    const firstEl = document.getElementById('1').innerHTML;
+    const secEl = document.getElementById('2').innerHTML;
+    const third = document.getElementById('3').innerHTML;
+    const fourthEl = document.getElementById('4').innerHTML;
+    const fivEl = document.getElementById('5').innerHTML;
+    const sixEl = document.getElementById('6').innerHTML;
+    const sevEl = document.getElementById('7').innerHTML;
+    const eigthEl = document.getElementById('8').innerHTML;
+    const nineEl = document.getElementById('9').innerHTML;
+
+    const firstCase = firstEl === side && secEl === side && third === side;
+    const secondCase = fourthEl === side && fivEl === side && sixEl === side;
+    const thirdCase = sevEl === side && eigthEl === side && nineEl === side;
+
+    const fourthCase = firstEl === side && fourthEl === side && sevEl === side;
+    const fifthCase = secEl === side && fivEl === side && eigthEl === side;
+    const sixthCase = third === side && sixEl === side && nineEl === side;
+
+    const seventhCase = firstEl === side && fivEl === side && nineEl === side;
+    const eighthCase = third === side && fivEl === side && sevEl === side;
+
+    const cases = [
+      firstCase,
+      secondCase,
+      thirdCase,
+      fourthCase,
+      fifthCase,
+      sixthCase,
+      seventhCase,
+      eighthCase,
+    ];
+
+    if (cases.find((c) => c === true)) {
+      return 'Você venceu!';
+    } return null;
+  };
+
   useEffect(() => {
-    enemy.attack();
+    const result = verifyTableResult();
+    if (result === 'Você venceu!') {
+      return window.setTimeout(() => alert(result), 500);
+    }
+    return enemy.attack();
   }, [round]);
 
   return (
--- src/service/TicTacToeEnemy.js
+++ src/service/TicTacToeEnemy.js
@@ -31,51 +31,11 @@ class Enemy extends Player {
       nineEl,
     ];
 
-    if (this.round === 1) {
-      const filter = elements.filter((el) => el.innerHTML === '');
-      const filteredId = filter.map((e) => Number(e.id));
-      const finalNumber = filteredId[Math.floor(Math.random() * filteredId.length)];
-      document.getElementById(JSON.stringify(finalNumber)).innerHTML = this.side;
-    }
+    const filter = elements.filter((el) => el.innerHTML === '');
+    const filteredId = filter.map((e) => Number(e.id));
+    const finalNumber = filteredId[Math.floor(Math.random() * filteredId.length)];
+    document.getElementById(JSON.stringify(finalNumber)).innerHTML = this.side;
   }
 }
 
 export default Enemy;
-
-// const verifyTableResult = () => {
-//     const firstEl = document.getElementById('1').innerHTML;
-//     const secEl = document.getElementById('2').innerHTML;
-//     const third = document.getElementById('3').innerHTML;
-//     const fourthEl = document.getElementById('4').innerHTML;
-//     const fivEl = document.getElementById('5').innerHTML;
-//     const sixEl = document.getElementById('6').innerHTML;
-//     const sevEl = document.getElementById('7').innerHTML;
-//     const eigthEl = document.getElementById('8').innerHTML;
-//     const nineEl = document.getElementById('9').innerHTML;
-
-//     const firstCase = firstEl === side && secEl === side && third === side;
-//     const secondCase = fourthEl === side && fivEl === side && sixEl === side;
-//     const thirdCase = sevEl === side && eigthEl === side && nineEl === side;
-
-//     const fourthCase = firstEl === side && fourthEl === side && sevEl === side;
-//     const fifthCase = secEl === side && fivEl === side && eigthEl === side;
-//     const sixthCase = third === side && sixEl === side && nineEl === side;
-
-//     const seventhCase = firstEl === side && fivEl === side && nineEl === side;
-//     const eighthCase = third === side && fivEl === side && sevEl === side;
-
-//     const cases = [
-//       firstCase,
-//       secondCase,
-//       thirdCase,
-//       fourthCase,
-//       fifthCase,
-//       sixthCase,
-//       seventhCase,
-//       eighthCase,
-//     ];
-
-//     if (cases.find((c) => c === true)) {
-//       console.log('Ganhou!');
-//     }
-//   };
