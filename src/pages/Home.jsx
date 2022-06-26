cd9323ffc4063467ff21b9e375fec315b3f6e5ad
Lukita <lucmaieski@gmail.com>
2022-06-26 19:15:59 -0300

    Adiciona estilo para pagina principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
   const navigate = useNavigate();
@@ -9,28 +10,32 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>MENU DE JOGOS</h1>
-      <h3>Escolha um jogo:</h3>
-      <div>
-        <h2>Jogo da Velha</h2>
-        <button
-          onClick={(e) => handleGameOption(e.target.id)}
-          id="JogoDaVelha"
-          type="button"
-        >
-          Jogar
-        </button>
+    <div className="home-body">
+      <div className="home-header">
+        <h1>MENU DE JOGOS</h1>
+        <h3>Escolha um jogo:</h3>
       </div>
-      <div>
-        <h2>Pedra Papel ou Tesoura</h2>
-        <button
-          onClick={(e) => handleGameOption(e.target.id)}
-          id="Jokenpo"
-          type="button"
-        >
-          Jogar
-        </button>
+      <div className="home-games-body">
+        <div className="home-tictactoe-body">
+          <button
+            className="home-game-btn"
+            onClick={(e) => handleGameOption(e.target.id)}
+            id="JogoDaVelha"
+            type="button"
+          >
+            JOGO DA VELHA
+          </button>
+        </div>
+        <div className="home-jokenpo-body">
+          <button
+            className="home-game-btn"
+            onClick={(e) => handleGameOption(e.target.id)}
+            id="Jokenpo"
+            type="button"
+          >
+            PEDRA PAPEL OU TESOURA
+          </button>
+        </div>
       </div>
     </div>
   );
