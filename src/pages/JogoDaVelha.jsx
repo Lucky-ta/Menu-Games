670f8748693634f395c62e2336562ba364d81308
Lukita <lucmaieski@gmail.com>
2022-06-26 02:55:34 -0300

    Implementa funcionalidade de resetar jogo

--- src/pages/JogoDaVelha.jsx
+++ src/pages/JogoDaVelha.jsx
@@ -2,14 +2,17 @@
 import React, { useContext, useEffect, useState } from 'react';
 import '../styles/TicTacToe.css';
 import { HiArrowSmLeft } from 'react-icons/hi';
+import { useNavigate } from 'react-router-dom';
 import Player from '../service/TicTacToePlayer';
 import Enemy from '../service/TicTacToeEnemy';
 import TicTacToeScore from '../components/TicTacToeScore';
 import MyContext from '../MyContext/MyContext';
 import TicTacToePickSide from '../components/TicTacToePickSide';
 import verifyTableResult from '../service/ticTacToeVerifyResult';
+import clearTable from '../service/TicTacToeClearTable';
 
 function JogoDaVelha() {
+  const navigate = useNavigate();
   const tableLength = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const {
@@ -31,10 +34,18 @@ function JogoDaVelha() {
 
   const hanldeAttack = (id) => {
     if (side === null) return alert('Selecione um lado!');
-    if (document.getElementById(id).innerHTML !== '') { return alert('Você não pode jogar aqui!'); }
+    if (document.getElementById(id).innerHTML !== '') {
+      return alert('Você não pode jogar aqui!');
+    }
     setPlayedPositions([...playedPositions, Number(id)]);
     setRound(round + 1);
     return player.attack(id);
+  };
+
+  const resetGame = () => {
+    SetEnemyPoints(0);
+    SetPlayerPoints(0);
+    clearTable();
   };
 
   useEffect(() => {
@@ -47,7 +58,13 @@ function JogoDaVelha() {
   return (
     <div className="tictactoe-body">
       <div className="toctactoe-header">
-        <button className="tictactoe-arrow-btn" type="button"><HiArrowSmLeft size="2.8em" /></button>
+        <button
+          onClick={() => navigate('/')}
+          className="tictactoe-arrow-btn"
+          type="button"
+        >
+          <HiArrowSmLeft size="2.8em" />
+        </button>
         <h1 className="tictactoe-title">Jogo Da Velha</h1>
       </div>
       {side === null ? (
@@ -71,7 +88,7 @@ function JogoDaVelha() {
           />
         ))}
       </div>
-      <button className="tictactoe-reset-btn" type="button">
+      <button onClick={resetGame} className="tictactoe-reset-btn" type="button">
         RESETAR
       </button>
     </div>
